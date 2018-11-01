8363c3709498e7178401679b57116f1aad9dad6d
s40723240 <s40723240@mde.tw>
2018-11-01 16:14:07 +0800

    final update

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = 'KMOL'
+AUTHOR = 'mdecp2018'
 SITENAME = '機械設計工程系s40723240分組網誌'
 # 不要用文章所在目錄作為類別
 USE_FOLDER_AS_CATEGORY = False
